1b1f250a60c50bc6702bda6566113f9e500ebfa0
mary <marynes0111@gmail.com>
2022-10-14 11:16:41 -0700

    filled issue #22

--- src/main/java/io/github/codexrm/jris/ConferencePaper.java
+++ src/main/java/io/github/codexrm/jris/ConferencePaper.java
@@ -3,6 +3,13 @@ package io.github.codexrm.jris;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+/**
+ * Represents an conferencePaper Reference
+ * Please see the {@link io.github.codexrm.jris.BaseReference}
+ *
+ * @author Marynes Diaz
+ * @version 1.0
+ */
 public class ConferencePaper extends BaseReference {
 
     private final ArrayList<String> authorList;
@@ -20,39 +27,119 @@ public class ConferencePaper extends BaseReference {
         editorList = new ArrayList<>();
     }
 
+    /**
+     * Gets the conferencePaper’s author list.
+     *
+     * @return the conferencePaper’s authorList.
+     */
     public ArrayList<String> getAuthorList() { return authorList; }
 
+    /**
+     * Sets the conferencePaper’s author.
+     *
+     * @param author A String containing the conferencePaper’s author.
+     */
     public void setAuthor(final String author) {
         this.authorList.add(author);
     }
 
+    /**
+     * Gets the conferencePaper’s editor list.
+     *
+     * @return the conferencePaper’s editorList.
+     */
     public ArrayList<String> getEditorList() { return editorList; }
 
+    /**
+     * Sets the conferencePaper’s editor.
+     *
+     * @param editor A String containing the conferencePaper’s editor.
+     */
     public void setEditor(final String editor) {
         this.editorList.add(editor);
     }
 
+    /**
+     * Gets the conferencePaper’s title.
+     *
+     * @return the conferencePaper’s title.
+     */
     public String getTitle() { return title; }
 
+    /**
+     * Sets the conferencePaper’s title.
+     *
+     * @param title A String containing the conferencePaper’s title.
+     */
     public void setTitle(final String title) { this.title = title; }
 
+    /**
+     * Gets the conferencePaper’s date.
+     *
+     * @return the conferencePaper’s date.
+     */
     public LocalDate getDate() { return date; }
 
+    /**
+     * Sets the conferencePaper’s date.
+     *
+     * @param date A LocalDate containing the conferencePaper’s date.
+     */
     public void setDate(final LocalDate date) { this.date = date; }
 
+    /**
+     * Gets the conferencePaper’s volume.
+     *
+     * @return the conferencePaper’s volume.
+     */
     public String getVolume() { return volume; }
 
+    /**
+     * Sets the conferencePaper’s volume.
+     *
+     * @param volume A String containing the conferencePaper’s volume.
+     */
     public void setVolume(final String volume) { this.volume = volume; }
 
+    /**
+     * Gets the conferencePaper’s publisher.
+     *
+     * @return the conferencePaper’s publisher.
+     */
     public String getPublisher() { return publisher; }
 
+    /**
+     * Sets the conferencePaper’s publisher.
+     *
+     * @param publisher A String containing the conferencePaper’s publisher.
+     */
     public void setPublisher(final String publisher) { this.publisher = publisher; }
 
+    /**
+     * Gets the conferencePaper’s address.
+     *
+     * @return the conferencePaper’s address.
+     */
     public String getAddress() { return address; }
 
+    /**
+     * Sets the conferencePaper’s address.
+     *
+     * @param address A String containing the conferencePaper’s address.
+     */
     public void setAddress(final String address) { this.address = address; }
 
+    /**
+     * Gets the conferencePaper’s pages.
+     *
+     * @return the conferencePaper’s pages.
+     */
     public String getPages() { return pages; }
 
+    /**
+     * Sets the conferencePaper’s pages.
+     *
+     * @param pages A String containing the conferencePaper’s pages.
+     */
     public void setPages(final String pages) { this.pages = pages; }
 }
--- src/main/java/io/github/codexrm/jris/WebPage.java
+++ src/main/java/io/github/codexrm/jris/WebPage.java
@@ -3,6 +3,13 @@ package io.github.codexrm.jris;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+/**
+ * Represents an webPage Reference
+ * Please see the {@link io.github.codexrm.jris.BaseReference}
+ *
+ * @author Marynes Diaz
+ * @version 1.0
+ */
 public class WebPage extends BaseReference  {
 
     private final ArrayList<String> authorList;
@@ -16,25 +23,75 @@ public class WebPage extends BaseReference  {
         authorList = new ArrayList<>();
     }
 
+    /**
+     * Gets the webPage’s author list.
+     *
+     * @return the webPage’s authorList.
+     */
     public ArrayList<String> getAuthorList() { return authorList; }
 
+    /**
+     * Sets the webPage’s author.
+     *
+     * @param author A String containing the webPage’s author.
+     */
     public void setAuthor(final String author) {
         this.authorList.add(author);
     }
 
+    /**
+     * Gets the webPage’s title.
+     *
+     * @return the webPage’s title.
+     */
     public String getTitle() { return title; }
 
+    /**
+     * Sets the webPage’s title.
+     *
+     * @param title A String containing the webPage’s title.
+     */
     public void setTitle(final String title) { this.title = title; }
 
+    /**
+     * Gets the webPage’s date.
+     *
+     * @return the webPage’s date.
+     */
     public LocalDate getDate() { return date; }
 
+    /**
+     * Sets the webPage’s date.
+     *
+     * @param date A LocalDate containing the webPage’s date.
+     */
     public void setDate(final LocalDate date) { this.date = date; }
 
+    /**
+     * Gets the webPage’s accessDate.
+     *
+     * @return the webPage’s accessDate.
+     */
     public LocalDate getAccessDate() { return accessDate; }
 
+    /**
+     * Sets the webPage’s accessDate.
+     *
+     * @param accessDate A LocalDate containing the webPage’s accessDate.
+     */
     public void setAccessDate(final LocalDate accessDate) { this.accessDate = accessDate; }
 
+    /**
+     * Gets the webPage’s url.
+     *
+     * @return the webPage’s url.
+     */
     public String getUrl() { return url; }
 
+    /**
+     * Sets the webPage’s url.
+     *
+     * @param url A String containing the webPage’s url.
+     */
     public void setUrl(final String url) { this.url = url; }
 }
